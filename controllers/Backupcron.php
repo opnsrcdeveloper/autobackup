70093901e0bf669970100504ae701a2f7149089d
opnsrcdeveloper <gurbinder@1wayit.com>
2016-12-29 18:18:25 +0530

    dfeww

--- controllers/Backupcron.php
+++ controllers/Backupcron.php
@@ -1,59 +1,43 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
-class Backupcron extends CI_Controller {
+class Backupcron extends CI_Controller 
+{
 
    	public function __construct()
 	{
 		parent::__construct();
 		$this->load->model('backupcron_model');
 	}
-		
-	public function index(){
-		$pastdays = "-3 days"; // past days from today to delete backup 
-		
-	$db = $this->backupcron_model->database();
-	$date = date('d-m-y', time()); 
-	$folder = 'DB_Backup/';
-	$filename = $folder."db-".$db."-".$date; 
 	
-	if(file_exists($filename.'.sql') ==1 )
+	public function dobckup() 
 	{
-		echo "Backup Already Exists with filname ".$filename;
-		exit;		
+		$folder = 'DB_Backup/';
+		if (!is_dir($folder))
+		mkdir($folder, 0777, true);
+		chmod($folder, 0777);
+
+		$pastdays = "-3 days"; // past days from today to delete backup 
+		$db = $this->backupcron_model->db_details(); // get db details 
+		$date = date('d-m-y', time());
+		$sdate = date('d-m-y',  strtotime($pastdays) );
+		$folder = 'DB_Backup';		
+		$filename = $folder."\db-".$db['database']."-".$date.'.sql';
+		$old_filename = $folder."\db-".$db['database']."-".$sdate.'.sql';
+		
+		if(file_exists($filename) ==1 )
+		{
+			echo "Backup Already Exists with filname ".$filename;
+			exit;		
+		}
+		else
+		{
+			$cmd = 'D:\xampp\mysql\bin\mysqldump --user=' .$db['username'].' --password=  '. $db['password'] .'     --host='.$db['host'].'  '.$db['database'].' > '.FCPATH.$filename;
+			system($cmd,$return);
+			if( $return == 0 )
+			{
+				if( file_exists($old_filename)  == 1)
+				unlink($old_filename);
+			}
+		}
 	}
-
-	$return = $this->backupcron_model->all_tables();
-
-
-	
-// Create Backup Folder
-$folder = 'DB_Backup/';
-if (!is_dir($folder))
-mkdir($folder, 0777, true);
-chmod($folder, 0777);
-
-$date = date('d-m-y', time()); 
-
-$filename = $folder."db-".$db."-".$date; 
-$sdate = date('d-m-y',  strtotime($pastdays) );
-$old_filename = $folder."db-".$db."-".$sdate; 
-
-$handle = fopen($filename.'.sql','w+');
-$status = fwrite($handle,$return);
-if($status !== false)
-{
-	echo "Database backup Success, db-".$db."-".$sdate." file is saved to DB_Backup folder in your root directory";
-	if ( file_exists($filename.'.sql') == 1)
-	{ 
-		if( file_exists($old_filename.'.sql')  == 1)
-		unlink($old_filename.'.sql');
-	}
-}
-
-fclose($handle);
-}
-
-
-
-
 }
